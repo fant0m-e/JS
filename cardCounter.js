ac421ce32bd2217091849a13ec48cf5c31b8b5fa
Tristan <tcassel@protonmail.com>
2021-08-01 14:51:24 -0700

    Update cardCounter.js

--- cardCounter.js
+++ cardCounter.js
@@ -1,7 +1,6 @@
 var count = 0;
 
 function cc(card) {
-  // Only change code below this line
 
   switch(card) {
     case 2:
@@ -25,8 +24,6 @@ function cc(card) {
   } else if (count <= 0) {
     return count + " Hold";
   }
-
-  // Only change code above this line
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
