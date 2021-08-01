3092248ecf9be814babbbd8bc610b63adc3d1783
Tristan <tcassel@protonmail.com>
2021-08-01 14:49:15 -0700

    basic JS programs
    cardCounter could be useful paired with some front end work.

---
+++ golfScores.js
@@ -0,0 +1,23 @@
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+
+  if (strokes == 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes == par - 1) {
+    return "Birdie";
+  } else if (strokes == par) {
+    return "Par";
+  } else if (strokes == par + 1) {
+    return "Bogey";
+  } else if (strokes == par + 2) {
+    return "Double Bogey";
+  } else if (strokes >= par + 3) {
+    return "Go Home!";
+  }
+  return "Change Me";
+
+}
+
+golfScore(5, 4);
