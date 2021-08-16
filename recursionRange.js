d10598794c280c895345a2cf09aadb97a777e801
Tristan <tcassel@protonmail.com>
2021-08-16 16:12:09 -0700

    Create recursionRange.js

---
+++ recursionRange.js
@@ -0,0 +1,33 @@
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum - startNum === 0) {
+    return [startNum];
+  } else {
+    var numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+};
+
+// 
+//
+// next solution
+//
+//
+
+function rangeOfNumbers(startNum, endNum) {
+  return startNum === endNum
+    ? [startNum]
+    : rangeOfNumbers(startNum, endNum - 1).concat(endNum);
+}
+
+//
+//
+// next solution
+//
+//
+
+function rangeOfNumbers(startNum, endNum) {
+  return startNum === endNum
+    ? [startNum]
+    : [...rangeOfNumbers(startNum, endNum - 1), endNum ];
+}
